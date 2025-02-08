41ae514a7c4527db9d7c8139c8108fd07e7b2de0
shandohowell <shandohowell94@gmail.com>
2025-02-08 10:48:04 -0500

    Worked on Home Page and the header component.

--- app/page.js
+++ app/page.js
@@ -1,14 +1,36 @@
 import Link from "next/link";
 
+import ImageSlideshow from "@/components/Images/ImageSlideshow";
+import classes from "./page.module.css"
+
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Mealitant
-      </h1>
-      <p><Link href="/meals">Meals</Link></p>
-      <p><Link href="/meals/share">Share Meal</Link></p>
-      <p><Link href="/community">Community</Link></p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Mealitant</h1>
+            <p>Taste and Share food from around the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>What is Mealitant?</h2>
+          <p>
+            Mealitant is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
