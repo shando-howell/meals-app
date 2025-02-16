177e7e892ca53ef2a42df128b70a6f6e6ef42c70
shandohowell <shandohowell94@gmail.com>
2025-02-15 22:18:54 -0500

    Worked on meals database.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/Meals/MealsGrid';
+import { getMeals } from '@/lib/meals';
 
-const Meals = () => {
+const Meals = async () => {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +21,7 @@ const Meals = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
       </main>
     </>
   )
